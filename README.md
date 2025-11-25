728bb283e4ac45d46582fa3558d78eb2c46a3a17
unknown <andradanendrabagas@gmail.com>
2025-11-26 06:53:46 +0700

    pesan

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
 1. cd ke demo terlebih dahulu
 2. lalu run dengan npm run dev
 3. setelah itu ubah http://localhost:3000/Main/Home
-   
--- demo/app/page.tsx
+++ demo/app/page.tsx
@@ -1,6 +1,5 @@
 import { redirect } from "next/navigation";
 
 export default function RootPage() {
-  // Langsung arahkan pengguna ke /Main/Home
   redirect("/Main/Home");
 }
